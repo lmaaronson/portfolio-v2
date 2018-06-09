f6cc483d4c247a354f60a1036f1ab104f238eccc
Laurie Aaronson <aaronson.laurie@gmail.com>
2018-06-08 22:59:36 -0500

    updated some css

--- src/components/projects.js
+++ src/components/projects.js
@@ -16,60 +16,78 @@ class Projects extends Component {
           
           
           {/* Project 1 */}
-          <Card shadow={2} style={{width: '320px', height: '320px', margin: 'auto'}}>
+          <Card shadow={4} style={{width: '320px', height: '320px', margin: 'auto'}}>
+         
           <CardTitle
-            className="card-title"
+            className="card-title align-center"
             style={{
-              color: 'black',
-              height: '175px',
-              background: 'url(http://i1.kym-cdn.com/entries/icons/original/000/018/385/Rs_634x1024-130605092844-634.DespMe2.mh.060513.jpg) center / cover'
-            }}
-          >
-            Click-It
+              color: 'white',
+              height: '150px',
+              background: 'black',
+              // 'url(https://image.flaticon.com/icons/svg/69/69084.svg) center / cover'
+            }}>
+            Project: Click-It
           </CardTitle>
+
           <CardText>
-            A memory game. Everytime an image is clicked, they move randomly to another position and a point is scored.  If an image is clicked twice the game is over and the user loses.  
+            A memory game. Click an image earn a point. Images move randomly.  If an image is clicked twice the game is over and the user loses.  
           </CardText>
             <CardActions border className='align-center'>
-            <a href='https://github.com/lmaaronson/click-it' rel='noopener noreferrer' target='_blank'><Button colored>GitHub</Button></a>
-            <a href='https://lmaaronson.github.io/' rel='noopener noreferrer' target='_blank'>  <Button colored>Live Demo</Button></a>
+              <a href='https://github.com/lmaaronson/click-it' rel='noopener noreferrer' target='_blank'><Button colored>GitHub</Button></a>
+              <a href='https://lmaaronson.github.io/' rel='noopener noreferrer' target='_blank'>  <Button colored>Live Demo</Button></a>
             </CardActions>
             <CardMenu style={{color: '#fff'}}>
-              <IconButton name="share" />
+              {/* <IconButton name="share" /> */}
             </CardMenu>
           </Card>
           
           
           {/* Project 2 */}
-          <Card shadow={2} style={{width: '320px', height: '320px', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #2</CardTitle>
+          <Card shadow={4} style={{width: '320px', height: '320px', margin: 'auto'}}>
+            <CardTitle 
+            className="card-title align-center"
+            style={{
+              color: '#fff', 
+              height: '150px',
+              background: 'black' }}
+              // background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} 
+              >
+              Project: Bootcamp Bookmarks
+              </CardTitle>
             <CardText>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+             Five (5) day group project.  
+             Envisioned the inital concept for the app to solve a problem of organizing resoureces.  
+             Developed the backend models, routes and created intial wireframes for app UI.
             </CardText>
-            <CardActions border>
-              <Button colored>GitHub</Button>
-              <Button colored>CodePen</Button>
-              <Button colored>Live Demo</Button>
+            <CardActions border className='align-center'>
+            <a href='https://github.com/DMoldenhauer/Bookmarks' rel='noopener noreferrer' target='_blank'><Button colored>GitHub</Button></a>
+            <a href='https://bootcamp-bookmarks.herokuapp.com' rel='noopener noreferrer' target='_blank'> <Button colored>Live Demo</Button></a>
             </CardActions>
             <CardMenu style={{color: '#fff'}}>
-              <IconButton name="share" />
+              {/* <IconButton name="share" /> */}
             </CardMenu>
           </Card>
           
           
           {/* Project 3 */}
-          <Card shadow={2} style={{width: '320px', height: '320px', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #3</CardTitle>
+          <Card shadow={4} style={{width: '320px', height: '320px', margin: 'auto'}}>
+            <CardTitle 
+            className="card-title align-center"
+            style={{
+              color: '#fff', 
+              height: '176px',
+              background: 'black'
+              }} >
+              Project: Sequelize
+              </CardTitle>
             <CardText>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+              Users have the option to add or "eat" a burger.  App uses MySQL and Sequelize to store and retreive data, Express, and Node JS.
             </CardText>
-            <CardActions border>
+            <CardActions border className="align-center">
               <Button colored>GitHub</Button>
               <Button colored>CodePen</Button>
-              <Button colored>Live Demo</Button>
             </CardActions>
             <CardMenu style={{color: '#fff'}}>
-              <IconButton name="share" />
             </CardMenu>
           </Card>
         </div>
@@ -85,7 +103,6 @@ class Projects extends Component {
     return(
       <div>
         <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
-          <Tab>React</Tab>
           <Tab>HTML/CSS</Tab>
          
         </Tabs>
